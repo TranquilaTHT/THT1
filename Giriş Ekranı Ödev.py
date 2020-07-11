2087133b1969e702187b37f47c4e82b1b6e577c6
Eren <49488668+erenyilmazz@users.noreply.github.com>
2020-07-11 18:24:38 +0300

    Add files via upload

---
+++ Giriş Ekranı Ödev.py
@@ -0,0 +1,13 @@
+def giris():
+    kullanıcı= input("Kullanıcı Adını Giriniz: ")
+    sifre=input("Şifreyi Giriniz: ")
+
+    if (kullanıcı=="Rozzy") and (sifre=="crazyboygittigeldi_42"):
+        print("hg krdşm")
+    else:
+        print("Hatalı giriş")
+
+while (1==1):
+    giris()
+
+
